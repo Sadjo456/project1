7d5cbe9c2520f1a396fb68b19190133a395b91b0
Aaron Liu <aaron155658@gmail.com>
2022-07-24 19:48:58 -0400

    Homepage api w/imgs,pords,prices

--- assest/js/script.js
+++ assest/js/script.js
@@ -2,6 +2,7 @@
 var fakeStoreUrl = "https://fakestoreapi.com/products";
 var productEl = document.querySelector("#prod-elem");
 var categories = document.querySelectorAll(".category");
+var productCardEl = document.querySelector("#product-card");
 
 function testing() {
   categories.forEach((category) => {
@@ -22,6 +23,37 @@ function getProduct() {
   fetch(fakeStoreUrl).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
+
+      //get product title, price, and image
+      data.forEach((product) => {
+        var productImage = product.image;
+        var productName = product.title;
+        var productPrice = product.price;
+
+        // create element
+        var cardSection = document.createElement("div");
+        var cardImage = document.createElement("img");
+        var cardName = document.createElement("h5");
+        var cardPrice = document.createElement("h5");
+
+        //append card to container
+        cardSection.setAttribute("class", "card column is-3");
+        productEl.append(cardSection);
+
+        cardImage.setAttribute("src", productImage);
+        cardImage.setAttribute("class", "card-image image is-128x128 column");
+        cardSection.append(cardImage);
+
+        cardName.setAttribute("class", "card-content column");
+        cardSection.append(cardName);
+
+        cardPrice.setAttribute("class", "card-content column");
+        cardSection.append(cardPrice);
+
+        cardImage.innerHTML = productImage;
+        cardName.innerHTML = productName;
+        cardPrice.innerHTML = "$" + productPrice;
+      });
     });
   });
 }
